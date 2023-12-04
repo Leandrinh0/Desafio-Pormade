602bf83bd5b6b0b89e96e6c6687f38ef0d875b2f
RobertCrysthian <kedadefps@gmail.com>
2023-12-04 17:30:22 -0300

    Responsividade na página de edição de produtos

--- src/Pages/EditProducts/index.jsx
+++ src/Pages/EditProducts/index.jsx
@@ -6,6 +6,9 @@ import { useContext, useState } from "react";
 import { MenuContext } from "../../Contexts/MenuContext";
 import { CgList } from "react-icons/cg";
 import ModalEditProducts from "../../ComponentsAdmin/ModalEditProducts";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { Link } from "react-router-dom";
+import ItemTable from "../../Components/ItemTable(cellphone)";
 
 
 
@@ -14,17 +17,17 @@ import ModalEditProducts from "../../ComponentsAdmin/ModalEditProducts";
 
 export default function EditProducts() {
 
-    const {newUser, setNewUser} = useContext(MenuContext)
+    const { newUser, setNewUser } = useContext(MenuContext)
     const [showDeleteModal, setShowDeleteModal] = useState(false)
-    const {deletar, setDeletar} = useContext(MenuContext)
-    const {newProduct, setNewProduct} = useContext(MenuContext)
+    const { deletar, setDeletar } = useContext(MenuContext)
+    const { newProduct, setNewProduct } = useContext(MenuContext)
     const [openEditForm, setOpenEditForm] = useState(false)
 
     return (
         <div className='w-screen h-screen flex flex-col items-center'>
-            <div className='flex w-full h-14 justify-between mt-6'>
+            <div className='flex w-full h-14 justify-around mt-6 '>
                 <button
-                    className='text-white_pormade bg-light_green rounded-lg w-1/6 h-9 text-2xl flex justify-center ml-36'
+                    className='text-white_pormade bg-light_green  hover:bg-green_button rounded-lg w-1/6 h-9 text-2xl flex justify-center items-center tablet:w-1/5 tablet:ml-8'
                     onClick={() => setNewProduct(!newProduct)}
                 >
                     <div className='flex justify-center w-full items-center'>
@@ -32,13 +35,14 @@ export default function EditProducts() {
                         <p>Cadastrar</p>
                     </div>
                 </button>
-                <div className='w-2/6 h-2/3 flex justify-evenly items-center bg-black_modal border-2 border-light_green rounded-md'>
+                <div className='w-2/6 h-2/3 flex justify-evenly items-center bg-black_modal border-2 border-light_green rounded-md tablet:w-2/5'>
                     <input placeholder='Pesquisar...' className='w-10/12 h-full outline-none text-white bg-black_modal text-lg' />
                     <FaSearch className='w-6 h-6 text-light_green' />
                 </div>
             </div>
-            <div className='w-10/12 bg-black h-5/6 border-2 border-light_green rounded-2xl'>
-                <table className='w-full text-center'>
+            <div className='w-10/12 bg-black h-5/6 border-2 border-light_green rounded-2xl flex flex-col justify-between tablet:ml-14 almostCellphone:ml-0 
+                almostCellphone:w-11/12 almostCellphone:border-none almostCellphone:p-2 almostCellphone:justify-normal'>
+                <table className='w-full text-center almostCellphone:hidden'>
                     <thead className='bg-light_green rounded-tl-lg rounded-tr-3xl'>
                         <tr>
                             <td className='rounded-tl-lg border-r-2 border-black text-bold text-2xl'>ID</td>
@@ -49,18 +53,18 @@ export default function EditProducts() {
                     </thead>
                     <tbody>
                         <tr>
-                            <td className='border-r border-t border-b border-light_green text-2xl'>Iaasdasd</td>
+                            <td className='border-r border-t border-b border-light_green text-2xl'>01</td>
                             <td className='border border-light_green text-2xl py-2'>adsdsa</td>
                             <td className='border border-light_green text-2xl py-2'>adsdasd</td>
                             <td
                                 className='border border-light_green text-2xl py-2'
                             >
                                 <div className='flex justify-between items-center mx-3'>
-                                    <div className='flex justify-around w-2/6 items-center'>
+                                    <div className='flex justify-around w-2/6 items-center tablet:w-3/5 tablet:justify-between'>
                                         <CgList className='w-10 h-10'/>
-                                        <ImPencil
-                                            onClick={() => setOpenEditForm(!openEditForm)} 
-                                            className='w-8 h-8 cursor-pointer'
+                                        <ImPencil 
+                                            className='w-8 h-8 cursor-pointer tablet:mr-1'
+                                            onClick={() => setOpenEditForm(!openEditForm)}
                                         />
                                     </div>
                                     <MdDeleteForever 
@@ -72,10 +76,55 @@ export default function EditProducts() {
                         </tr>
                     </tbody>
                 </table>
+                <div className=' justify-center mt-4 tablet:ml-16 flex items-center mb-10 almostCellphone:hidden'>
+                    <IoIosArrowBack className='w-9 h-9 text-white_pormade cursor-pointer' />
+                    <div>
+                        <Link className='text-4xl text-white_pormade hover:bg-green_pormade px-2'>1</Link>
+                        <Link className='text-4xl text-white_pormade hover:bg-green_pormade px-2'>2</Link>
+                        <Link className='text-4xl text-white_pormade hover:bg-green_pormade px-2'>3</Link>
+                        <Link className='text-4xl text-white_pormade hover:bg-green_pormade px-2'>...</Link>
+                        <Link className='text-4xl text-white_pormade hover:bg-green_pormade px-2'>9</Link>
+                    </div>
+                    <IoIosArrowForward className='w-9 h-9 text-white_pormade cursor-pointer' />
+                </div>
+
+                {/* Mobile */}
+                <div className='hidden justify-center p-2 almostCellphone:flex'>
+                    <button
+                        className='text-white_pormade bg-light_green rounded-xl  h-10 text-2xl flex justify-center items-center w-full'
+                        onClick={() => setNewProduct(!newProduct)}
+                    >
+                        <div className='flex justify-center w-full items-center'>
+                            <FaPlus className='text-white mr-4' />
+                            <p>Cadastrar</p>
+                        </div>
+                    </button>
+                </div>
+                <ItemTable secondRowItem={"Preço"} secondRowValue={"R$10,00"} openModalProp={openEditForm} setOpenModalProp={setOpenEditForm}/>
+                <ItemTable secondRowItem={"Preço"} secondRowValue={"R$10,00"} openModalProp={openEditForm} setOpenModalProp={setOpenEditForm}/>
+                <ItemTable secondRowItem={"Preço"} secondRowValue={"R$10,00"} openModalProp={openEditForm} setOpenModalProp={setOpenEditForm}/>
             </div>
-            <ModalDelete word='produto'/>
-            <ModalEditProducts newProduct={openEditForm} setNewProduct={setOpenEditForm}/>
-            
+
+            {/* Celular -> */}
+            <div className=' justify-center hidden mt-2 items-center mb-2 almostCellphone:flex'>
+                <IoIosArrowBack className='w-6 h-6 text-white_pormade cursor-pointer' />
+                <div>
+                    <Link className='text-2xl text-white_pormade hover:bg-green_pormade px-2'>1</Link>
+                    <Link className='text-2xl text-white_pormade hover:bg-green_pormade px-2'>2</Link>
+                    <Link className='text-2xl text-white_pormade hover:bg-green_pormade px-2'>3</Link>
+                    <Link className='text-2xl text-white_pormade hover:bg-green_pormade px-2'>...</Link>
+                    <Link className='text-2xl text-white_pormade hover:bg-green_pormade px-2'>9</Link>
+                </div>
+                <IoIosArrowForward className='w-6 h-6 text-white_pormade cursor-pointer' />
+            </div>
+
+
+
+
+
+            <ModalDelete word='produto' />
+            <ModalEditProducts newProduct={openEditForm} setNewProduct={setOpenEditForm} />
+
         </div>
     )
 }
