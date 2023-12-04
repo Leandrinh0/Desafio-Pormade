d7d425f0ffa2ff28625d4e3d596fff1135750a6e
RobertCrysthian <kedadefps@gmail.com>
2023-12-04 09:28:05 -0300

    Elementos da home totalmente responsivos
    Adicionei responsividade para mobile e tablet no componente histórico

--- src/Components/CardHistoric/index.jsx
+++ src/Components/CardHistoric/index.jsx
@@ -15,26 +15,26 @@ const CardHistoric = () => {
     }
 
     return (
-        <div className="cardContainer h-2/5 pb-4 bg-black_pormade rounded-xl border border-light_green block tablet:h-3/6 tablet:mb-4">
-                <div className="w-11/12 h-1/6 border-b border-light_green text-white text-xl font-bold mt-2 ">
+        <div className="cardContainer h-2/4 pb-4 my-2 bg-black_pormade rounded-xl border border-light_green block tablet:h-3/6 tablet:w-full almostCellphone:border-none almostCellphone:w-full almostCellphone:mb-2 almostCellphone:h-1/4 almostCellphone:my-0">
+                <div className="w-11/12 h-1/6 border-b border-light_green text-white text-xl font-bold mt-2 almostCellphone:text-lg almostCellphone:border-none">
                   Pedido - 00
                 </div>
                 <div className="w-11/12 h-3/5 mt-2 tablet:h-2/4">
-                    <div className="w-full h-1/3 flex justify-between tablet:h-8">
-                        <label className="font-medium text-xl tablet:text-base">{window.screen.width < 1100? "Quantidade" : "Quantidade de Produtos"}</label> 
-                        <label className="font-medium text-xl tablet:text-base">4</label>   
+                    <div className="w-full h-1/3 flex justify-between tablet:h-8 almostCellphone:h-6">
+                        <label className="font-medium text-xl tablet:text-base almostCellphone:text-sm">{window.screen.width < 1100? "Quantidade" : "Quantidade de Produtos"}</label> 
+                        <label className="font-medium text-xl tablet:text-base almostCellphone:text-sm">4</label>   
                     </div> 
-                    <div className="w-full h-1/3 flex justify-between tablet:h-12">
-                        <label className="font-medium text-xl tablet:text-base">Valor Total</label> 
-                        <label className="font-medium text-xl tablet:text-base">R$20,00</label>   
+                    <div className="w-full h-1/3 flex justify-between tablet:h-12 almostCellphone:h-6">
+                        <label className="font-medium text-xl tablet:text-base almostCellphone:text-sm">Valor Total</label> 
+                        <label className="font-medium text-xl tablet:text-base almostCellphone:text-sm">R$20,00</label>   
                     </div> 
-                    <div className="w-full h-1/3 flex justify-between tablet:flex-col tablet:items-center">
-                        <label className="font-medium text-xl tablet:text-base">Data da compra</label> 
-                        <label className="font-medium text-xl tablet:text-base">30/09/2023</label>   
+                    <div className="w-full h-1/3 flex justify-between tablet:flex-col tablet:items-center almostCellphone:flex-row almostCellphone:h-4">
+                        <label className="font-medium text-xl tablet:text-base almostCellphone:text-sm">Data da compra</label> 
+                        <label className="font-medium text-xl tablet:text-base almostCellphone:text-sm">30/09/2023</label>   
                     </div>    
                 </div>
-                <div className="w-11/12 text-right ">
-                    <label className="text-light_green flex justify-end font-bold text-xl hover:cursor-pointer hover:underline tablet:justify-center tablet:text-lg tablet:mt-2 tablet:underline tablet:underline-offset-8:" onClick={redirect}>Ver Mais</label>
+                <div className="w-11/12 text-right almostCellphone:flex almostCellphone:justify-end">
+                    <label className="text-light_green flex justify-end font-bold text-xl hover:cursor-pointer hover:underline tablet:justify-center tablet:text-lg tablet:mt-2 tablet:underline tablet:underline-offset-2 almostCellphone:text-base almostCellphone:mt-2" onClick={redirect}>Ver Mais</label>
                 </div>                         
         </div>
     )
--- src/Components/ModalHistoric/index.jsx
+++ src/Components/ModalHistoric/index.jsx
@@ -16,7 +16,7 @@ const ModalHistoric = () => {
     return (
         <>
         <div className={`fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-20 duration-200 justify-center items-center ${historic ? "flex" : "hidden"}`}>
-                <div className="w-2/3 h-4/5 bg-black_modal rounded-3xl border-2 border-light_green tablet:h-5/6">
+                <div className="w-10/12 h-4/5 bg-black_modal rounded-3xl border-2 border-light_green tablet:h-5/6 almostCellphone:w-5/6">
                     <div className='w-full flex justify-end p-4'>
                         <AiOutlineClose
                             className='text-light_green w-8 h-8 cursor-pointer'
@@ -25,7 +25,7 @@ const ModalHistoric = () => {
                 </div>
                 <div className='w-full h-full flex flex-col items-center '>
 
-                    <div className="flex flex-row justify-between flex-wrap w-5/6 h-5/6">
+                    <div className="flex flex-row justify-between flex-wrap w-5/6 h-5/6 almostCellphone:flex-col almostCellphone:flex-nowrap almostCellphone:w-full almostCellphone:px-2">
 
                     <CardHistoric />
                     <CardHistoric />
