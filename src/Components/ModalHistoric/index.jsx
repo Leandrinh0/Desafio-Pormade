2c8d0acb787cf8c94c462f24ceab05a451407462
RobertCrysthian <kedadefps@gmail.com>
2023-12-04 11:21:40 -0300

    Quase total responsividade na página de detalhes_p

--- src/Components/ModalHistoric/index.jsx
+++ src/Components/ModalHistoric/index.jsx
@@ -16,8 +16,8 @@ const ModalHistoric = () => {
     return (
         <>
         <div className={`fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-20 duration-200 justify-center items-center ${historic ? "flex" : "hidden"}`}>
-                <div className="w-10/12 h-4/5 bg-black_modal rounded-3xl border-2 border-light_green tablet:h-5/6 almostCellphone:w-5/6">
-                    <div className='w-full flex justify-end p-4'>
+                <div className="w-10/12 h-5/6 bg-black_modal rounded-3xl p-4 border-2 border-light_green tablet:h-5/6 almostCellphone:w-5/6">
+                    <div className='w-full h-8 flex justify-end items-start'>
                         <AiOutlineClose
                             className='text-light_green w-8 h-8 cursor-pointer'
                             onClick={close}
--- src/Pages/ProductDetails/index.jsx
+++ src/Pages/ProductDetails/index.jsx
@@ -1,19 +1,22 @@
 import { FiArrowLeftCircle } from "react-icons/fi";
 import CardProductDetails from "../../Components/cardProductDetails";
 import {useNavigate} from 'react-router-dom'
+import { IoCloseSharp } from "react-icons/io5";
 
 
 export default function ProductDetails () {
     const navigate = useNavigate()
 
     return(
-        <div className="w-full h-screen  flex flex-col justify-center items-center">
-            <div className='flex items-center w-10/12 m-3'>
+        <div className="w-full h-screen  flex flex-col justify-center items-center almostCellphone:bg-black">
+            <div className='flex items-center w-10/12 m-3 almostCellphone:hidden'>
                 <FiArrowLeftCircle className='w-14 h-14 text-light_green ml-5 cursor-pointer' onClick={() => navigate(-1)} />
                 <h1 className='font-bold text-3xl ml-3' >Detalhes do Pedido</h1>
             </div>
-            <div className="w-10/12 h-5/6 flex justify-center flex-wrap bg-black border border-light_green rounded-3xl">
-                <CardProductDetails/>
+            <div className="w-10/12 h-5/6 flex justify-center flex-wrap bg-black border-2 border-light_green rounded-3xl tablet:flex-col tablet:flex-nowrap tablet:items-center tablet:justify-start tablet:pb-10">
+                <div className='w-full h-6 justify-end mr-2 mt-1 hidden almostCellphone:flex'>
+                    <IoCloseSharp className='text-light_green w-10 h-10'/>
+                </div>
                 <CardProductDetails/>
                 <CardProductDetails/>
                 <CardProductDetails/>
