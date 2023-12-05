01f4a7c1e565e2b06b9004c0d6c04f41d85211c3
RobertCrysthian <kedadefps@gmail.com>
2023-12-05 16:02:25 -0300

    Adicionando paginação funcional

--- src/Components/CardProduct/index.jsx
+++ src/Components/CardProduct/index.jsx
@@ -4,7 +4,7 @@ import '../../index.css'
 
 
 
-export default function CardProduct() {
+export default function CardProduct({name, price, description}) {
     return(
         <div 
             className='w-4/12 bg-black_pormade border border-green_pormade rounded-2xl flex flex-col p-5 my-4 mx-10 
@@ -12,14 +12,14 @@ export default function CardProduct() {
             almostCellphone:w-full almostCellphone:p-2 almostCellphone:mx-0 almostCellphone:my-0 almostCellphone:mb-2 almostCellphone:border-none'
         >
             <div className="flex justify-between items-center border-b border-b-green_pormade mb-2">
-                <h1 className='font-bold text-2xl py-1 almostCellphone:text-xl'>Product Name</h1>
+                <h1 className='font-bold text-2xl py-1 almostCellphone:text-xl'>{name}</h1>
                 <FaStar className='w-7 h-7 text-yellow'/>
             </div>
             <div className='w-10/12 almostCellphone:w-11/12 tablet:w-11/12'>
-                <p className='text-sm almostCellphone:text-xs almostCellphone:text-grey_text almostCellphone:h-12'>Description, description, description, description, Description, description, description</p>
+                <p className='text-sm almostCellphone:text-xs almostCellphone:text-grey_text almostCellphone:h-12'>{description}</p>
             </div>
             <div className='flex justify-between items-center mt-3 almostCellphone:items-end'>
-                <p className='font-bold	text-2xl almostCellphone:text-lg'>R$99,00</p>
+                <p className='font-bold	text-2xl almostCellphone:text-lg'>R${price}</p>
                 <FiPlusCircle className='w-10 h-10 mr-3 text-light_green almostCellphone:w-8 almostCellphone:h-8 almostCellphone:mr-0'/>
             </div>
         </div>
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,26 +1,22 @@
 import axios from "axios"
 import { useEffect, useState, useContext } from "react"
 import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
-import { MdOutlineShoppingCart, MdOutlineWatchLater, MdOutlineRemoveShoppingCart } from "react-icons/md";
-import { RxExit } from "react-icons/rx";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 
-import logo from "../../assets/images/Vector.png"
-import adminLogo from "../../assets/images/adminlogo.png"
 import { Link } from "react-router-dom";
 import CardProduct from "../../Components/CardProduct";
 import { AdminContext } from "../../Contexts/AdminContext";
 import { MenuContext } from "../../Contexts/MenuContext";
-import Modal from "../../Components/Modal";
-import CartItem from "../../Components/CartItem";
 import { FaSearch } from "react-icons/fa";
-import { LuMenu } from "react-icons/lu";
+import api from "../../http/api";
+
 
 
 const Home = () => {
 
     const [productData, setProductData] = useState([])
+    const [allProduts, setAllProducts] = useState([])
 
 
     const { show } = useContext(AdminContext)
@@ -39,19 +35,49 @@ const Home = () => {
     }
 
     const navigate = useNavigate()
+    const params = useParams()
 
     const exit = () => {
         return navigate('/login')
     }
 
-    useEffect(() => {
-        axios.get('192.168.155.6:3002/produtos/lista')
-            .then(response => setProductData(response.data))
+    const fetchData = async () => {
+        await api.get(`/produtos/lista?pagina=${(convertedParams-1)}&&itens=8`)
+            .then(response => setProductData(response.data.products))
+
+        await api.get('/produtos/lista')
+            .then(response => setAllProducts(response.data.products))
+    }
+
+    useEffect(() =>{
+        fetchData()
     }, [])
 
     console.log(productData)
 
+    const convertedParams = parseInt(params.id)
+    const [firstNav, setFirstNav] = useState(convertedParams)
+    const [secondNav, setSecondNav] = useState(convertedParams+1)
+    const [thirdNav, setThirdNav] = useState(convertedParams+2)
 
+
+    const nextPage = () => {
+        if (parseInt(params.id) < Math.round(allProduts.length/8)) {
+            navigate(`/home/${convertedParams+1}`)
+            api.get(`/produtos/lista?pagina=${(convertedParams)}&&itens=8`)
+            .then(response => setProductData(response.data.products))
+        }
+    }
+
+    const previousPage = () => {
+        if (parseInt(params.id) > 1) {
+            navigate(`/home/${convertedParams-1}`)
+            api.get(`/produtos/lista?pagina=${(convertedParams-2)}&&itens=8`)
+            .then(response => setProductData(response.data.products))
+        }
+    }
+
+    console.log(productData)
     return (
         <div className="w-full h-full flex flex-col justify-center items-center py-10 almostCellphone:mt-12">
 
@@ -66,33 +92,33 @@ const Home = () => {
                     </div>
                 </div>
                 <div className='flex flex-row justify-center items-center flex-wrap w-10/12 almostCellphone:flex-col almostCellphone:w-full almostCellphone:p-2'>
-                    <CardProduct />
-                    <CardProduct />
-                    <CardProduct />
-                    <CardProduct />
-                    <CardProduct />
-                    <CardProduct />
-                    <CardProduct />
-                    <CardProduct />
-                    <CardProduct />
+                    {productData.map((item) => {
+                        return <CardProduct description={item.description} name={item.name} price={item.value} key={item.id}/>
+                    })}
                 </div>
                 <footer className='flex justify-center my-4 tablet:hidden almostCellphone:hidden'>
-                    <IoIosArrowBack className='w-9 h-9 text-white_pormade cursor-pointer' />
+                    <IoIosArrowBack 
+                        className='w-9 h-9 text-white_pormade cursor-pointer' 
+                        onClick={() => previousPage()}
+                    />
                     <div>
-                        <Link className='text-4xl text-white_pormade hover:bg-green_pormade px-2'>1</Link>
-                        <Link className='text-4xl text-white_pormade hover:bg-green_pormade px-2'>2</Link>
-                        <Link className='text-4xl text-white_pormade hover:bg-green_pormade px-2'>3</Link>
-                        <Link className='text-4xl text-white_pormade hover:bg-green_pormade px-2'>...</Link>
-                        <Link className='text-4xl text-white_pormade hover:bg-green_pormade px-2'>9</Link>
+                        <Link className={`text-4xl text-white_pormade hover:bg-green_pormade px-2 ${firstNav === parseInt(params.id)? "bg-light_green" : ""}`}>{firstNav}</Link>
+                        <Link className={`text-4xl text-white_pormade hover:bg-green_pormade px-2 ${secondNav === parseInt(params.id)? "bg-light_green" : ""}`} onClick={() => nextPage()} >{secondNav}</Link>
+                        <Link className={`text-4xl text-white_pormade hover:bg-green_pormade px-2 ${thirdNav === parseInt(params.id)? "bg-light_green" : ""}`}>{thirdNav}</Link>
+                        <Link className={`text-4xl text-white_pormade hover:bg-green_pormade px-2`}>...</Link>
+                        <Link className={`text-4xl text-white_pormade hover:bg-green_pormade px-2 `}>{Math.round(allProduts.length/8)}</Link>
                     </div>
-                    <IoIosArrowForward className='w-9 h-9 text-white_pormade cursor-pointer' />
+                    <IoIosArrowForward 
+                        className='w-9 h-9 text-white_pormade cursor-pointer' 
+                        onClick={() => nextPage()}
+                    />
                 </footer>
             </div>
 
             <footer className=' justify-center mt-4 tablet:ml-16 hidden tablet:flex almostCellphone:flex almostCellphone:ml-0'>
                     <IoIosArrowBack className='w-9 h-9 text-white_pormade cursor-pointer' />
                     <div>
-                        <Link className='text-4xl text-white_pormade hover:bg-green_pormade px-2'>1</Link>
+                        <Link className={`text-4xl text-white_pormade hover:bg-green_pormade px-2`}>1</Link>
                         <Link className='text-4xl text-white_pormade hover:bg-green_pormade px-2'>2</Link>
                         <Link className='text-4xl text-white_pormade hover:bg-green_pormade px-2'>3</Link>
                         <Link className='text-4xl text-white_pormade hover:bg-green_pormade px-2'>...</Link>
