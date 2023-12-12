f253cea9308d920b3d52769dfd3098acf42999e8
RobertCrysthian <kedadefps@gmail.com>
2023-12-12 17:54:56 -0300

    Refatorando paginação

--- src/Components/pagination/index.jsx
+++ src/Components/pagination/index.jsx
@@ -1,105 +1,68 @@
 import { useContext, useEffect, useState } from "react";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
-import { Link, useNavigate} from "react-router-dom";
+import { Link, useLocation, useNavigate, useParams} from "react-router-dom";
 import api from "../../http/api";
 import { AuthenticateContext } from "../../Contexts/Authenticate";
 
-export default function Pagination ({convertedParams, params, allItems, ItemData, setItemData, fetchData, urlNavigate, urlRequest, }) {
-    const navigate = useNavigate()
+export default function Pagination ({ allItems, ItemData, setItemData, fetchData, urlNavigate, urlRequest, }) {
+    
 
-    const [firstNav, setFirstNav] = useState(convertedParams)
-    const [secondNav, setSecondNav] = useState(convertedParams+1)
-    const [thirdNav, setThirdNav] = useState(convertedParams+2)
-    const lastPage = Math.round((allItems.length/8) + 1)
+    const lastPage = Math.ceil((allItems.length/8))
     const {user} = useContext(AuthenticateContext)
+    const location = useLocation()
+    const navigate = useNavigate()
+    const params = useParams()
+    const convertedParams = parseInt(params.id)
+    const [actualPage, setActualPage] = useState(1)
+    const maxItens=8;
+
+
+    function fetchItens () {
+        api.get(`/produtos/lista?pagina=${actualPage}&&itens=${maxItens}`)
+        .then((res) => setItemData(res.data.products))
+        .catch(window.alert)
+    }
+
 
     useEffect(() => {
-        fetchData()
-    },[])
-
-
-    const nextPage = () => {
-        if (parseInt(params.id) < Math.round((allItems.length/8 + 1))) {
-            if(urlRequest === "produtos") {
-                navigate(`/${urlNavigate}/${convertedParams+1}`)
-                api.get(`/${urlRequest}/lista?pagina=${(convertedParams)}&&itens=8`)
-                .then(response => setItemData(response.data.products))
-                .then(setFirstNav(convertedParams+1), setSecondNav(convertedParams+2), setThirdNav(convertedParams+3))
-            }
-            else {
-                navigate(`/${urlNavigate}/${convertedParams+1}`)
-                api.post(`/${urlRequest}/lista?pagina=${(convertedParams)}&&itens=8`,{
-                    cpf:user.cpf
-                })
-                .then(response => setItemData(response.data.persons))
-                .then(setFirstNav(convertedParams+1), setSecondNav(convertedParams+2), setThirdNav(convertedParams+3))
-
-            }
-        }
-    }
+        navigate(`/home/${actualPage}`)
+        fetchItens()
+    },[actualPage])
 
     const previousPage = () => {
-        if (parseInt(params.id) > 1) {
-            if(urlRequest === "produtos") {
-                navigate(`/${urlNavigate}/${convertedParams-1}`)
-                api.get(`/${urlRequest}/lista?pagina=${(convertedParams-2)}&&itens=8`)
-                .then(response => setItemData(response.data.products))
-                .then(setFirstNav(convertedParams-1), setSecondNav(convertedParams), setThirdNav(convertedParams+1))
-            }
-            else {
-                navigate(`/${urlNavigate}/${convertedParams-1}`)
-                api.post(`/${urlRequest}/lista?pagina=${(convertedParams-2)}&&itens=8`,{
-                    cpf:user.cpf
-                })
-                .then(response => setItemData(response.data.persons))
-                .then(setFirstNav(convertedParams-1), setSecondNav(convertedParams), setThirdNav(convertedParams+1))
-            }
+        if(actualPage > 1){
+            setActualPage(actualPage-1)
         }
     }
-    const NavigateLastPage = () => {
-        if(convertedParams !== lastPage){
-
-            if(urlRequest === "produtos") {navigate(`/${urlNavigate}/${lastPage}`)
-                api.get(`/${urlRequest}/lista?pagina=${(convertedParams+lastPage-2)}&&itens=8`)
-                .then(response => setItemData(response.data.products))
-                .then(setFirstNav(convertedParams+(lastPage - 1)), setSecondNav(convertedParams+lastPage), setThirdNav(convertedParams+lastPage))
-            }
-            else {
-                    navigate(`/${urlNavigate}/${lastPage}`)
-                    api.post(`/${urlRequest}/lista?pagina=${(convertedParams+lastPage-2)}&&itens=8`,{
-                        cpf:user.cpf
-                    })
-                    .then(response => setItemData(response.data.persons))
-                    .then(setFirstNav(convertedParams), setSecondNav(convertedParams+1), setThirdNav(convertedParams+1))
-            }
-        }
-
-
-    }
-
-
-
 
 
     return(
         <>
-            <footer className={`flex justify-center my-4 tablet:hidden almostCellphone:hidden`}>
-            <IoIosArrowBack 
-                className='w-9 h-9 text-white_pormade cursor-pointer' 
-                onClick={() => previousPage()}
-            />
-            <div>
-                <button className={`text-4xl text-white_pormade hover:bg-green_pormade px-2 ${firstNav === parseInt(params.id)? "bg-light_green" : ""}`}>{firstNav}</button>
-                <button disabled={secondNav > lastPage} className={`text-4xl text-white_pormade hover:bg-green_pormade px-2 ${secondNav === parseInt(params.id)? "bg-light_green" : ""} disabled:text-grey_pormade disabled:hover:bg-black`} onClick={() => nextPage()} >{secondNav}</button>
-                <button disabled={thirdNav > lastPage} className={`text-4xl text-white_pormade hover:bg-green_pormade px-2 ${thirdNav === parseInt(params.id)? "bg-light_green" : ""} disabled:text-grey_pormade disabled:hover:bg-black`} onClick={() => nextPage()}>{thirdNav}</button>
-                <button className={`text-4xl text-white_pormade hover:bg-green_pormade px-2`}>...</button>
-                <button className={`text-4xl text-white_pormade hover:bg-green_pormade px-2`} onClick={() => NavigateLastPage()}>{Math.round((allItems.length/8) + 1)}</button>
-            </div>
-            <IoIosArrowForward 
-                className='w-9 h-9 text-white_pormade cursor-pointer' 
-                onClick={() => nextPage()}
-            />
-            </footer>
+            
+                <div >
+                <IoIosArrowBack 
+                    className='w-9 h-9 text-white_pormade cursor-pointer' 
+                    onClick={() => previousPage}
+                />
+                {Array(5).fill('').map((_,index) => {
+                    return (
+                        <button 
+                            key={index}
+                            className={`text-4xl text-white_pormade hover:bg-green_pormade px-2 `}
+                            onClick={() => setActualPage(index + 1)}
+                            >
+                            {index + 1}
+                        </button>
+                    )
+                })
+                }
+                <IoIosArrowForward 
+                    className='w-9 h-9 text-white_pormade cursor-pointer' 
+                    onClick={() => setActualPage(actualPage + 1)}
+                />
+
+                </div>
+            
         </>
 
 
